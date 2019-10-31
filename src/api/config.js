141729d72984f15f40e6b782ce6a7d3cc1b8cc76
sanyuan0704 <39261479+sanyuan0704@users.noreply.github.com>
2019-10-31 20:03:54 +0800

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseUrl = "http://47.105.150.105/m-api";
+export const baseUrl = "http://47.98.159.95/m-api";
 
 // axios的实例及拦截器配置
 const axiosInstance = axios.create({
